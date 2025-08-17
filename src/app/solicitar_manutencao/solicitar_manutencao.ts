99797e272d1b834851a23a66c087917e0e754972
bruno-pedron <brunorupaner09@gmail.com>
2025-08-17 19:16:58 -0300

    add tela solicitar manutencao + fix routes

--- src/app/solicitar_manutencao/solicitar_manutencao.ts
+++ src/app/solicitar_manutencao/solicitar_manutencao.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface SolicitacaoManutencao {
+  id?: number;
+  descEquipamento: string;
+  categEquipamento: string;
+  descDefeito: string;
+  dataHora: Date;
+  status: 'ABERTA';
+}
 
 @Component({
-  selector: 'app-soliciar_manutencao',
-  imports: [],
+  selector: 'app-solicitarmanutencao',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './solicitar_manutencao.html',
-  styleUrl: './solicitar_manutencao.css'
+  styleUrls: ['./solicitar_manutencao.css']
 })
-export class SolicitarManutencao {
 
+export class SolicitarManutencao{
+
+  manutencaoForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.manutencaoForm = new FormGroup({
+      desc_equipamento: new FormControl(null, Validators.required),
+      categ_equipamento: new FormControl(null, Validators.required),
+      desc_defeito: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit(): void {
+    if (this.manutencaoForm.valid) {
+      const novaSolicitacao: SolicitacaoManutencao = {
+        descEquipamento: this.manutencaoForm.value.desc_equipamento,
+        categEquipamento: this.manutencaoForm.value.categ_equipamento,
+        descDefeito: this.manutencaoForm.value.desc_defeito,
+        dataHora: new Date(),
+        status: 'ABERTA'
+      };
+
+      console.log('teste:', novaSolicitacao);
+      
+      this.manutencaoForm.reset();
+    } else {
+      console.log('formulario invalido');
+    }
+  }
 }
