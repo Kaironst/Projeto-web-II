2ed046d52845d7d5978978aa5e8206e6e2890ba9
Darkitinhus-Cebolitinhus3862 <128605786+Darkitinhus-Cebolitinhus3862@users.noreply.github.com>
2025-08-16 22:32:06 -0300

    componente de login e botão para exibir ou esconder ele SEM css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  mostrarLogin = false;
+
+  exibirLogin() {
+    this.mostrarLogin = !this.mostrarLogin;
+  }
+
   protected readonly title = signal('projetoWeb2');
+  
 }
