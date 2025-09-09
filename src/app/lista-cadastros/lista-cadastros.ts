087c01e24654de457a1968bc268885c3013ceeea
kaironst <pedronovak4@gmail.com>
2025-09-08 22:50:15 -0300

    adicionado funcionalidade do bloco de listar membros, pelo menos inicialmente.

--- src/app/lista-cadastros/lista-cadastros.ts
+++ src/app/lista-cadastros/lista-cadastros.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { CadastroService, CadastroTable } from '../services/cadastroService';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list'
 
 @Component({
   selector: 'app-lista-cadastros',
-  imports: [],
+
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule],
+
   templateUrl: './lista-cadastros.html',
   styleUrl: './lista-cadastros.css'
 })
 export class ListaCadastros {
+
+  cadastroService = inject(CadastroService);
+  cadastros: CadastroTable[] = [];
+
+  //usa subscribe para passar o valor ao array cadastros
+  ngOnInit() {
+    this.cadastroService.getAllCadastros().subscribe(listaCadastro => {
+      this.cadastros = listaCadastro;
+    });
+  }
+
 
 }
