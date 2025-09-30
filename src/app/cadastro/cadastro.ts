bd70279f5280a33e54f6f34b7c4d8672399fa9c9
Darkitinhus-Cebolitinhus3862 <128605786+Darkitinhus-Cebolitinhus3862@users.noreply.github.com>
2025-09-30 06:49:21 -0300

    Api do cep no cadastro
    vou arrumar a formatação do cadastro depois pq agr ta esticado

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -2,13 +2,13 @@ import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
-
 import { MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardActions, MatCardContent } from "@angular/material/card";
 import { ClienteUtil, Cliente } from '../services/DBUtil/cliente-util';
 import { ControlaForm } from '../services/controla-form';
+import { CepService } from '../services/cep';
 
 @Component({
   selector: 'app-cadastro',
@@ -38,5 +38,31 @@ export class Cadastro {
     }
   }
 
+  cepControl = new FormControl('');
+  rua = '';
+  bairro = '';
+  cidade = '';
+  uf = '';
+
+  constructor(private cepService: CepService) {}
+
+  onCepChange() {
+    const cep = this.cepControl.value?.replace(/\D/g, '');
+    if (cep?.length === 8) {
+      this.cepService.buscarCep(cep).subscribe({
+        next: (dados) => {
+          if (!dados.erro) {
+            this.rua = dados.logradouro;
+            this.bairro = dados.bairro;
+            this.cidade = dados.localidade;
+            this.uf = dados.uf;
+          }
+        },
+        error: (err) => {
+          console.error('Erro ao buscar CEP:', err);
+        }
+      });
+    }
+  }
 
 }
