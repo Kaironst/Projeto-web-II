e1ff0d36984dd7d8fd8135e3b93cc0447e289b52
GhostScoutw <ghostscoutw@gmail.com>
2025-10-13 23:00:55 -0300

    métodos do HASH + SALT, mas não integrado ainda. Porque o usuário não cria a própria senha? Entenderia se fosse pra funcionário, mas é pra isso que existe o CRUD de funcionário.

--- src/app/services/DBUtil/cliente-util.ts
+++ src/app/services/DBUtil/cliente-util.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ContatoComBanco } from './contato-com-banco';
+import { sha256 } from 'js-sha256';
 
 export interface Cliente {
   id?: number | null;
@@ -11,6 +12,7 @@ export interface Cliente {
   cep?: string | null;
   telefone?: string | null;
   senha?: string | null;
+  salt?: string | null;
 }
 
 @Injectable({
@@ -41,9 +43,23 @@ export class ClienteUtil extends ContatoComBanco {
     return this.http.delete<Cliente>(`${this.requestUrl}/${id}`);
   }
 
-  gerarSenha(): string {
+  gerarSenha(): string { //nao sei oque fazer com isso, porque que o usuario não faz sua própria senha?
     let intvalue = Math.floor(Math.random() * 10000);
     return intvalue.toString().padStart(4, '0');
   }
 
+  //validações de senha com HASH e SALT
+  senhaValida(input: string, cliente: Cliente): boolean {
+    if (!cliente.senha || !cliente.salt) return false;
+    const hashTentativa = sha256(input + cliente.salt);
+    return hashTentativa === cliente.senha;
+  }
+  gerarSalt(): string {
+    return crypto.getRandomValues(new Uint8Array(16))
+      .reduce((acc, byte) => acc + byte.toString(16).padStart(2, '0'), '');
+  }
+
+  gerarHashSenha(senha: string, salt: string): string {
+    return sha256(senha + salt);
+  }
 }
